3fdbe8a28e97c4078a653c53655033698089e0ac
Poyi Chen <poyi.design@gmail.com>
2014-07-22 14:35:35 -0700

    Initial Commit

---
+++ Pastery/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pastery
+//
+//  Created by Poyi Chen on 7/22/14.
+//  Copyright (c) 2014 Poyi. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Pastery/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Pastery
+//
+//  Created by Poyi Chen on 7/22/14.
+//  Copyright (c) 2014 Poyi. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
