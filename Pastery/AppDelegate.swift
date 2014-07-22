bfed603b68645c307374a636aee060cdecbaac45
Poyi Chen <poyi.design@gmail.com>
2014-07-22 16:31:20 -0700

    Setup Icon in Status Bar

--- Pastery/AppDelegate.swift
+++ Pastery/AppDelegate.swift
@@ -11,10 +11,31 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
                             
     @IBOutlet var window: NSWindow
+    
+    var statusItem: NSStatusItem
+    var menuItem: NSMenu?
+    
+    // Initialize the statusItem
+    init()
+    {
+        self.statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(CGFloat(NSSquareStatusItemLength))
+    }
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
         // Insert code here to initialize your application
+        
+        self.statusItem.title = nil
+        
+        // Define the icon image
+        var icon = NSImage(named: "Icon1")
+        icon.size = NSSize(width: 16, height: 16)
+        icon.setTemplate(true)
+        
+        // Set status image
+        self.statusItem.image = icon
+        statusItem.highlightMode = true
+        
     }
 
     func applicationWillTerminate(aNotification: NSNotification?) {
